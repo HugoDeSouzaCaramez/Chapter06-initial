340de6350c3c9df9e78cfbc8cd0bedbce0e1164d
HugoDeSouzaCaramez <hugo.7.musica@gmail.com>
2023-12-09 18:35:59 -0300

    Renderizando um componente React para teste

--- src/components/NavBar/NavBar.test.js
+++ src/components/NavBar/NavBar.test.js
@@ -1,3 +1,12 @@
+import { render } from '@testing-library/react';
+import NavBar from './NavBar';
+
 test('The NavBar component should render', () => {
-  expect(1 + 2).toBe('3');
+  const view = render(<NavBar />);
+  expect(view).toMatchSnapshot();
+});
+
+test('The NavBar component should render with a title', () => {
+  const view = render(<NavBar title="Test application #2" />);
+  expect(view).toMatchSnapshot();
 });
